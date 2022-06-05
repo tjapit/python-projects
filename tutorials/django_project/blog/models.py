7e44761c076bf620fb5f0381a8800e121c6483ad
Timothy Japit <timothy.japit@gmail.com>
2022-06-05 22:51:09 +0700

    Database and Migrations
    Created a Post model as a class that is translated into SQL by Django with its Object Relational Mapper (ORM), py manage.py makemigrations -> migrate. Use py manage.py sqlmigrations [app_folder_path] [migration_number] (i.e. py manage.py sqlmigrations blog 0001)

--- tutorials/django_project/blog/models.py
+++ tutorials/django_project/blog/models.py
@@ -1,3 +1,18 @@
+from time import time_ns
 from django.db import models
+from django.utils import timezone
+# importing pre-existing User model from django
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # auto_now=True: datetime of last modified
+    # auto_now_add=True: current datetime only when created **can't ever be updated
+    # default=timezone.now: current datetime but mutable
+    date_posted = models.DateTimeField(default=timezone.now)
+    # User @OneToMany Post
+    # on_delete=models.CASCADE: delete Post if User is deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+

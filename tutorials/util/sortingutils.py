d10171b150328921e9adb4c647a15602435197c2
Timothy Japit <timothy.japit@gmail.com>
2022-06-07 00:19:24 +0700

    Implemented, Tested Bubblesort and Restructured Test
    As title says, restructured test using self.assertListEqual() instead of testing contents 1-by-1 and a setUp() method, also added comments and docs

--- tutorials/util/sortingutils.py
+++ tutorials/util/sortingutils.py
@@ -1,4 +1,5 @@
 class SortingUtils:
+    """ Collection of sorting algorithms. All algorithms sort the given array in place. """
 
     @staticmethod
     def mergesort(array:list) -> list:
@@ -10,14 +11,21 @@ class SortingUtils:
             list:the sorted list
         """
         n:int = len(array)
+        # base case: array with only 1 element
         if n == 1:
             return array
+        # median index of array
         mid:int = n // 2
+        # left half of array
         left:list = array[:mid]
+        # right half of array
         right:list = array[mid:]
+        # recursive splitting of the left half
         SortingUtils.mergesort(left)
+        # recursive splitting of the right half
         SortingUtils.mergesort(right)
-        SortingUtils.__merge(left, right, array)
+        # return the merged array
+        return SortingUtils.__merge(left, right, array)
     
     def __merge(left:list, right:list, array:list) -> list:
         """Merges the left and right lists and returns a single list where the elements are sorted from the left and right lists.
@@ -30,8 +38,10 @@ class SortingUtils:
         Returns:
             list: sorted list from left and right
         """
+        # indices for left and right half of the original array respectively
         leftidx:int = 0
         rightidx:int = 0
+        # current index being filled on original array
         currentidx:int = leftidx + rightidx
         n:int = len(array)
         while currentidx < n:
@@ -50,6 +60,34 @@ class SortingUtils:
                 rightidx += 1
             # update current index on original array
             currentidx = leftidx + rightidx
-
+        # return sorted array
         return array
+
+    def bubblesort(array:list) -> list:
+        """Sorts the given list using bubblesort
+        
+        Args:
+            array (list): list to be sorted
+        Returns:
+            list:the sorted list
+        """
+        n:int = len(array)
+        # pass over the entire array until no swaps occur
+        repeat:bool = True
+        while repeat:
+            # assume no pass happens, and flip to True when a swap happens
+            repeat = False
+            for i in range(n - 1):
+                # if current value is larger than the one after,
+                if array[i] > array[i+1]:
+                    # swap the values (large values bubble to the top/end)
+                    temp:int = array[i]
+                    array[i] = array[i+1]
+                    array[i+1] = temp
+                    # a swap happened, repeat again
+                    repeat = True
+        # return sorted array
+        return array
+
+        
 
--- tutorials/util/test_sortingutils.py
+++ tutorials/util/test_sortingutils.py
@@ -1,13 +1,22 @@
 import unittest
 from sortingutils import SortingUtils
+import sys
 
 class TestCase(unittest.TestCase):
-    """Tests the mergesort implementation"""
-    def test_mergesort(self):
-        test_list = [2, 3, 1, 5, 4]
-        SortingUtils.mergesort(test_list)
-        for i in range(1, len(test_list) + 1):
-            self.assertEqual(i, test_list[i - 1])
+    """Tests the SortingUtils sort algorithms implementation"""
+
+    def setUp(self) -> None:
+        self.__test_list:list = [2, 3, 1, 5, 4]
+        self.__sorted_list:list = sorted(self.__test_list)
+        return super().setUp()
+        
+    def test_mergesort(self) -> None:
+        """ Tests mergesort algo """
+        self.assertListEqual(self.__sorted_list, SortingUtils.mergesort(self.__test_list))
+
+    def test_bubblesort(self) -> None:
+        """ Tests bubblesort algo """
+        self.assertListEqual(self.__sorted_list, SortingUtils.bubblesort(self.__test_list))
 
 if __name__ == '__main__':
     unittest.main()

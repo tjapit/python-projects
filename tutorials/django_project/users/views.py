bbd143938df86ccfeecdade53c44411969603952
Timothy Japit <timothy.japit@gmail.com>
2022-06-08 19:24:01 +0700

    Messages After User Registration
    Added checks to see if registration form data is valid, added redirection to homepage and a success message that can be viewed anywhere on base.html + styling with Bootstrap class alert-*

--- tutorials/django_project/users/views.py
+++ tutorials/django_project/users/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+    # if the request is "POST"
+    if request.method == "POST":
+        # then want to create a form with the data within the request.POST
+        form = UserCreationForm(request.POST)
+        # if form data is valid
+        if form.is_valid():
+            # form.cleaned_data is a dictionary containing data converted to python types
+            username = form.cleaned_data.get('username')
+            # flash message to indicate success in creating new account
+            messages.success(request, f'Account created for {username}!')
+            # redirect user to homepage name
+            return redirect('blog-home')
+    else:
+        # else return a blank page
+        form = UserCreationForm()
+
+    return render(request, 'users/register.html', {'title': 'Register', 'form': form})
+
+# # types of messages:
+# messages.debug
+# messages.info
+# messages.success
+# messages.warning
+# messages.error

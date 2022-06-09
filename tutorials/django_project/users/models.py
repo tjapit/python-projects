5ad8e6d13a92deda14a8d8ae3a544fd5d692dfc7
Timothy Japit <timothy.japit@gmail.com>
2022-06-09 21:56:51 +0700

    User Profile and Picture
    Created Profile model in models.py in users, added an image field to it, assigned a one-to-one relation between Profile-User, and registered the model to the admin site on admin.py

--- tutorials/django_project/users/models.py
+++ tutorials/django_project/users/models.py
@@ -1,3 +1,13 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    # One user has one profile and one profile has one user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Profile picture, default and directory that images are uploaded to when we upload a profile
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    # change default behavior when printing profile
+    def __str__(self) -> str:
+        return f'{self.user.username} Profile'

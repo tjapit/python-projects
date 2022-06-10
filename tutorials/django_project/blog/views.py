330132833c9b55220c9da260c7faef5b2fd6fdff
Timothy Japit <timothy.japit@gmail.com>
2022-06-11 02:53:07 +0700

    ListView and DetailView
    Created class-based views for the list of posts and a detailed view for each individual post,  PostListView doesn't follow convention for default ListView naming so the template name, context object name was changed, and post list ordered by most recently posted. PostDetailView follows the convention with the new template post_detail.html and 'object' as context. Cool thing is that it's possible to use variables in the routing, see urlpatterns for PostDetailView and its template

--- tutorials/django_project/blog/views.py
+++ tutorials/django_project/blog/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from typing import Optional, Union, Sequence
 from .models import Post
+
 
 def home(request):
     context = {
         'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    """ Class-based view: List, Detail, Create, Update, Delete """
+    # base model for class
+    model = Post
+
+    # default template naming scheme for class-based templates
+    template_name: str = 'blog/home.html' # <app>/<model>_<viewtype>.html
+
+    # default context name: object_list, change variable name to 'posts' because template already created
+    context_object_name: Optional[str] = 'posts'
+
+    # ordering Posts by most recently posted ('-' indicates reversed order)
+    ordering: Optional[Union[str, Sequence[str]]] =  '-date_posted'
+
+class PostDetailView(DetailView):
+    """ Class-based view following Django's convention """
+    model = Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})

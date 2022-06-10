4702a519a86aaba6eca0a701f69c0b026a646f60
Timothy Japit <timothy.japit@gmail.com>
2022-06-10 14:58:25 +0700

    Update User Profile: Resize Uploaded Images
    Added automated resizing when useded automated resizing for when user uploads a profile picture, and added author's profilepic to Posts on blog homepage

--- tutorials/django_project/users/models.py
+++ tutorials/django_project/users/models.py
@@ -1,13 +1,32 @@
 from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
+    # image max height and width
+    IMG_MAX_HEIGHT:int = 300
+    IMG_MAX_WIDTH:int = 300
+
     # One user has one profile and one profile has one user
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
     # Profile picture, default and directory that images are uploaded to when we upload a profile
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     # change default behavior when printing profile
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
+
+    def save(self):
+        """ Resize image if it is larger than max size before saving """
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > self.IMG_MAX_HEIGHT or img.width > self.IMG_MAX_WIDTH:
+            output_size = (self.IMG_MAX_WIDTH, self.IMG_MAX_HEIGHT)
+            # resize image
+            img.thumbnail(output_size)
+            # save to filepath and overwrite larger image
+            img.save(self.image.path)
